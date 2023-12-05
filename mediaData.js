444ae4934bafe9bb25288e4dbe9ff9368b7f2d83
diamondful <102664790+diamondful@users.noreply.github.com>
2023-12-05 10:36:10 +0800

    Add files via upload

---
+++ mediaData.js
@@ -0,0 +1,53 @@
+var mediaData = {
+    audioPath: "D:\\media\\mp3Files\\",
+    videoPath: "D:\\media\\mp4Files\\",
+    audioFileStruct: [{
+        audioFileName: "\\LinuxCMD-2.mp3",
+        title: ["开头", "了解文件系统树", "当前工作目录", "列出目录内容", "更改当前目录", "绝对路径", "相对路径", "关于文件名的重要事实",
+            "一些有用的快捷键", "总结"
+        ],
+        time: [
+            [1, 166],
+            [166, 808],
+            [808, 1370],
+            [1370, 1482],
+            [1482, 1646],
+            [1646, 2052],
+            [2052, 2558],
+            [2558, 3098],
+            [3098, 3311],
+            [3311, 3399]
+        ]
+    }, {
+        audioFileName: "\\LinuxCMD-0.mp3",
+    },{
+        audioFileName: "\\LinuxCMD-1.mp3",
+    },{
+        audioFileName: "\\LinuxCMD-3.mp3",
+    },{
+        audioFileName: "\\LinuxCMD-4.mp3",
+    },{
+        audioFileName: "\\LinuxCMD-5.mp3",
+    },
+
+],
+    videoFileStruct: [{
+        videoFileName: "\\west.mp4"
+    }, {
+        videoFileName: "\\funDogs.mp4"
+    }, {
+        videoFileName: "\\1.mp4"
+    }, {
+        videoFileName: "\\7.mp4"
+    }, {
+        videoFileName: "\\2.mp4"
+    }, {
+        videoFileName: "\\3.mp4"
+    }, {
+        videoFileName: "\\4.mp4"
+    }, {
+        videoFileName: "\\5.mp4"
+    }, {
+        videoFileName: "\\6.mp4"
+    }],
+} 
